3287ae43f28e662bfab4de21d2c172628af13989
xulihang <xulihanghai@163.com>
2025-03-05 21:25:23 +0800

    add detect handler for paddleocr's server

--- paddleOCR/server/server_paddleocr.py
+++ paddleOCR/server/server_paddleocr.py
@@ -46,6 +46,47 @@ def ocr():
         text_lines.append(text_line)
     os.remove(file_path)
     ret["text_lines"]=text_lines
+    return ret
+
+@route('/detect', method='POST')
+def detect():
+    upload = request.files.get('upload')
+    lang = request.forms.get('lang')
+    print(lang)
+    global current_lang
+    global ocr
+    if current_lang!=lang:
+        current_lang=lang
+        ocr = PaddleOCR(lang=current_lang)
+        
+    name, ext = os.path.splitext(upload.filename)
+    print(ext.lower())
+    if ext.lower() not in ('.png','.jpg','.jpeg'):
+        return "File extension not allowed."
+    timestamp=str(int(time.time()*1000))
+    savedName=timestamp+ext
+    save_path = "./uploaded/"
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    file_path = "{path}/{file}".format(path=save_path, file=savedName)
+    if os.path.exists(file_path)==True:
+        os.remove(file_path)
+    upload.save(file_path)        
+    ret = {}
+    result = ocr.ocr(file_path,rec=False,cls=True)[0]
+    print(result)
+    text_lines=[]
+    for line in result:
+        text_line={}
+        index=0
+        for coord in line:
+            text_line["x"+str(index)]=int(coord[0])
+            text_line["y"+str(index)]=int(coord[1])
+            index=index+1
+        text_line["text"]=""
+        text_lines.append(text_line)
+    os.remove(file_path)
+    ret["text_lines"]=text_lines
     return ret    
 
 
